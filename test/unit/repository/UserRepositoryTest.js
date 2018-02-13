37a9fb92a45fb3bc8f0e82250811782fd53e9806
samy-ca <samy.chrifialaoui@airbus.com>
2018-02-13 14:56:06 +0100

    tests

--- test/unit/repository/UserRepositoryTest.js
+++ test/unit/repository/UserRepositoryTest.js
@@ -45,3 +45,75 @@ describe("UserRepository", function() {
     });
 
 });
+
+
+
+describe("UserRepository FindOneById", function() {
+    it("should return existing user", function() {
+        var mockDb = jasmine.createSpyObj('db', ['get', 'find', 'value']);
+        mockDb.get.and.returnValue(mockDb);
+        mockDb.find.and.returnValue(mockDb);
+        mockDb.value.and.returnValue({
+            id: '7',
+            firstname: 'Cristiano',
+            lastname: 'Ronaldo',
+            birthday: '20-05-1990'
+        });
+        var repository = new UserRepository(mockDb);
+        var user = repository.findOneById('7');
+        expect(user.id).toEqual('7');
+        expect(user.firstname).toEqual('Cristiano');
+        expect(user.lastname).toEqual('Ronaldo');
+        expect(user.birthday).toEqual('20-05-1990');
+      });
+});
+
+
+
+describe("UserRepository Update", function() {
+    it("should update existing user", function(){
+        var mockDb = jasmine.createSpyObj('db', ['get', 'find','assign','write']);
+        mockDb.get.and.returnValue(mockDb);
+        mockDb.find.and.returnValue(mockDb);
+        mockDb.assign.and.returnValue(mockDb);
+
+
+        var repository = new UserRepository(mockDb);    
+        var user = repository.update({
+            id : 7,
+            firstname: 'Cristiano',
+            lastname : 'Ronaldo',
+            birthday : '20-05-1990'
+        });
+
+        expect(mockDb.write).toHaveBeenCalledTimes(1);
+    });
+});
+
+
+describe("UserRepository Delete",function(){
+     var mockDb = jasmine.createSpyObj('db', ['get', 'push', 'write','find','assign','remove']);
+     mockDb.get.and.returnValue(mockDb);
+     mockDb.push.and.returnValue(mockDb);
+     mockDb.find.and.returnValue(mockDb);
+     mockDb.assign.and.returnValue(mockDb);
+     mockDb.remove.and.returnValue(mockDb);
+     var repository = new UserRepository(mockDb);
+
+     repository.create({
+        id : 1,
+        firstname: 'Zinedine',
+        lastname : 'Zidane',
+        birthday : '15-01-1970'
+    });
+
+    //check if mockDb.remove is properly called
+    it("UserRepository.update should call mockDb.remove",function(){
+        repository.delete(1);
+        expect(mockDb.remove).toHaveBeenCalledWith({
+            'id' : 1
+        });
+        expect(mockDb.remove).toHaveBeenCalledTimes(1)
+    });
+        
+});
